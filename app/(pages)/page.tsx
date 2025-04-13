9155a85e28cbe5c7254bbf78f5138e741f29be58
SafidyRamaroson <safidyramaroson.patrick@gmail.com>
2025-04-13 09:53:23 +0300

    fetch vocabulary words from Dictionary API and update mock vocabulary state.

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -6,20 +6,44 @@ import { DailyVocabularyList } from "@/components/organisms/DailyVocabularyList"
 import { LayoutTemplate } from "@/components/templates";
 import { Button } from "@/components/ui/button";
 import { useGet } from "@/hooks/useGet";
-import { Download, Filter } from "lucide-react";
+import { Vocabulary } from "@/types";
+import { DictionaryApiResponse } from "@/types/apiResponse";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
 
-    const { data: randomWords, isPending } = useGet({
-        endpoint: "https://random-word-api.herokuapp.com/word?number=16",
+    const { data: randomWords, isPending } = useGet<DictionaryApiResponse[]>({
+        endpoint: "https://api.dictionaryapi.dev/api/v2/entries/en/free",
         queryKey: "wordsOfDay"
     });
 
-    const mockVocabularies = Array(16).fill({
-        term: "term",
-        definition: "definition",
-        pronuncitation: "pronunciation"
-    });
+    const [mockVocabularies, setMockVocabularies] = useState<Vocabulary[]>([]);
+
+    useEffect(() => {
+        if (isPending) {
+            console.log("Loading data...");
+        } else {
+            const mockVocabularies = Array(16).fill({
+                term: randomWords?.[0]?.word,
+                definition: randomWords?.[0]?.meanings[0]?.definitions[0]?.definition,
+                pronunciation: randomWords?.[0]?.phonetics[0]?.text,
+                audio: randomWords?.[0]?.phonetics[0]?.audio
+            });
+            console.log("mockVocabularies", mockVocabularies);
+            setMockVocabularies(mockVocabularies);
+        }
+    }, [randomWords]);
+
+    if(isPending){
+        return (
+            <LayoutTemplate>
+                <div className="flex flex-col items-center justify-center h-screen">
+                    <Button className="animate-pulse" label="Loading..."/>
+                </div>
+            </LayoutTemplate>
+        )
+    }
+
     return (
         <LayoutTemplate >
             <div className="flex flex-row items-center justify-between">
